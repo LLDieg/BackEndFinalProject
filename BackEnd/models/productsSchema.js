c2ad3a7a5feb5d964f502ea43db7aa3e77e79527
BarbaraPapa <103266205+BarbaraPapa@users.noreply.github.com>
2023-11-29 14:10:54 +0100

    modified productSchema

--- BackEnd/models/productsSchema.js
+++ BackEnd/models/productsSchema.js
@@ -9,11 +9,10 @@ const ProductsSchema = new Schema({
   dishes: [
     {
       dishName: { type: String, required: true },
-      description: { type: String, required: true },
       ingredients: { type: [String], required: true },
       price: { type: Number, required: true },
       image_url: { type: String, required: true },
-      rating: { type: Number, required: true },
+      
     },
   ],
 });
