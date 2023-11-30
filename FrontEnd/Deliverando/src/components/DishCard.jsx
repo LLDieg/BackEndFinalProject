00569d8389b7bd2a1e7398ddc5443b42076580d0
LLDieg <Diegollerena77@gmail.com>
2023-11-30 15:56:22 +0100

    added dishcard

---
+++ FrontEnd/Deliverando/src/components/DishCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+export default function DishCart() {
+
+
+
+  
+  return <div className="dishCard">
+<img src="#" alt="Dish picture"> </img>
+<h3>Dish Name</h3>
+<p>Price</p>
+
+
+  </div>;
+}
