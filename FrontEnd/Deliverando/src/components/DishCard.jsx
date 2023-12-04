ea60fa89629f3d5c8f2611bf1087626b337c1eb6
LLDieg <Diegollerena77@gmail.com>
2023-12-04 16:38:53 +0100

    trying to pull

--- FrontEnd/Deliverando/src/components/DishCard.jsx
+++ FrontEnd/Deliverando/src/components/DishCard.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
-
+import React, { useEffect } from "react";
 export default function DishCart() {
-  return (
-    <div>DishCart</div>
-  )
+const [ restaurant, setRestaurant ] = useState([]);
+
+useEffect(()=> {
+fetch("#")
+.then((res) => res.json())
+.then((data) => {
+  console.log(data);
+  setProducts(data.restaurant);
+});
+}, []);
+
+
+  return <div className="dishCard">
+<img src="#" alt="Dish picture"> </img>
+<h3>Dish Name</h3>
+<p>Price</p>
+  </div>;
 }
--- FrontEnd/Deliverando/src/components/RestaurantCard.jsx
+++ FrontEnd/Deliverando/src/components/RestaurantCard.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-
 export default function RestaurantCard() {
-  return (
-    <div>RestaurantCard</div>
+
+
+
+  
+  return (<div className="restaurantCard">
+  <img src="#" alt="Restaurant picture"> </img>
+  <h1>Restaurant Name</h1>
+  <p>Rating Symbol</p>
+  <p>Min: € </p>
+  <p>Delivery time | €2.00 Delivery Charge</p>
+    </div>
   )
 }
